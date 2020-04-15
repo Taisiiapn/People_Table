af62daf1155c7b85d8a69f700755552cc864bef1
Taisiia <taisiia.pomazan@gmail.com>
2020-04-16 02:11:55 +0300

    Updates

---
+++ precache-manifest.e43f26cfa18efaabb0e1625e621b5edf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43e0c5f614a4f14d72b377159ee51636",
+    "url": "/People_Table/index.html"
+  },
+  {
+    "revision": "9e61b957448862a535cd",
+    "url": "/People_Table/static/css/main.0d81c92f.chunk.css"
+  },
+  {
+    "revision": "4792aaae58daadedf0a7",
+    "url": "/People_Table/static/js/2.bda93e99.chunk.js"
+  },
+  {
+    "revision": "9e61b957448862a535cd",
+    "url": "/People_Table/static/js/main.4d67e1f8.chunk.js"
+  },
+  {
+    "revision": "bb5da4bdec5fad12f314",
+    "url": "/People_Table/static/js/runtime~main.1b293a59.js"
+  }
+]);
